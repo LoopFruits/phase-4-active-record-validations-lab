d0bf32c0a2c80ad15df0bb31cca47ad3bacee6c3
LoopFruits <Kensworldc@gmail.com>
2022-04-08 11:02:54 -0400

    commit tree 2d7714ee280fddf508ec8c8a8af247c86ebb04f2

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,12 @@
 class Author < ApplicationRecord
+    #validates name: cannont be empty/falsy
+    validates  :name, presence: true
+
+    #validate name is case-sensitive, why does this work? There must be a better way
+    validates_uniqueness_of :name
+
+    # phone number length is 10
+    validates :phone_number, length: { is: 10 }
+
+    
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+    #title cannont be empty/falsy
+    validates :title, presence: true
+    
+    # lenfth of content is at least 250
+    validates :content, length: { minimum: 250 }
+    #length of content is at most 250 
+    validates :summary, length: {  maximum: 250 }
+    #category is either fiction or non fiction c
+    # validates :category, inclusion: { in: }
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+
+    validates :title, presence: false
+
 end
